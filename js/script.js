bd82b421dde4d8c213351b8119f049c86a16320f
Alex Tyurin <alexturin@gmail.com>
2018-06-29 16:04:03 +0700

    Changes to be committed: 	modified:   index.html 	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-var num = 33721;
+var week = ["понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"];
 
-num = (3 * 3 * 7 *2 * 1);
-console.log(num);
-alert(Math.pow(num, 3));
+for (let i=0; i<7; i++) {
+	let day = week[i];
+
+	if ( i === 5 || i === 6){
+		day = '<strong>' + day + '</strong>';
+	};
+
+	let date = new Date();
+	let currentDay = date.getDay();
+	if ( i === currentDay - 1) {
+		day = '<i>' + day + '</i>';
+	}; 
+	document.writeln(day + '<br>');
+}
+
+let arr = ['23423452', '34534534', '456456', '56757567', '767678', '3345235345','67676878'];
+for(let i = 0; i < 7; i++) {
+	if( arr[i].charAt(0) == 3 || arr[i].charAt(0) == 7 ) {
+		console.log(arr[i]);
+	}
+}
